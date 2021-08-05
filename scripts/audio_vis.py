fb926afc8879c4a58ee403b452dbbf727649c303
Elias <eandualem@gmail.com>
2021-08-05 08:48:22 +0300

    feat: eda on audio transcription

--- scripts/audio_vis.py
+++ scripts/audio_vis.py
@@ -2,6 +2,11 @@ import numpy as np
 import librosa
 from log import get_logger
 import IPython.display as ipd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+from IPython.display import Image
 
 
 class AudioVis():
@@ -13,3 +18,13 @@ class AudioVis():
 
   def wav_plot(self, signal, sr=8000):
     return librosa.display.waveplot(signal, sr=sr)
+
+  def get_wc(self, df, stop_words):
+    plt.figure(figsize=(30, 20))
+    wordcloud = WordCloud(font_path='../fonts/NotoSansEthiopic-Medium.ttf', max_words=5000,
+                          background_color="salmon", width=3000, height=2000, colormap='Pastel1', 
+                          collocations=False, stopwords=stop_words).generate(' '.join(df.text.values))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.title('Most Frequent Words In Amharic Audio Transcription', fontsize=16)
+    plt.show()
