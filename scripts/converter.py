c3ad2b4e318b0eaaa72441b59473cabb995e30ef
deadex-ng <thisisfumba@gmail.com>
2021-08-07 10:46:31 +0200

    Add converter script

---
+++ scripts/converter.py
@@ -0,0 +1,25 @@
+import logging
+
+from pydub import AudioSegment
+
+I = logging.getLogger("pydub.converter")
+I.setLevel(logging.DEBUG)
+I.addHandler(logging.StreamHandler())
+
+class Converter():
+
+    def __init__(self):
+        self.audio_path = audio_path
+
+    def mono_to_sterio(self,audio_path):
+        audio = AudioSegment.from_wav(audio_path)
+        
+        #mono has 1 channel,sterio has 2 or more
+        if audio.channels == 2:
+            pass
+        else:
+            audio = audio.set_channels(2)
+        return audio
+
+
+            
