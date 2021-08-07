5f60d84b8329e704b156cf4e4342475b0e51e0b8
deadex-ng <thisisfumba@gmail.com>
2021-08-07 13:47:32 +0200

    Working on normalizer

--- scripts/normalizer.py
+++ scripts/normalizer.py
@@ -1,8 +1,9 @@
 import logging
 
 from pydub import AudioSegment
+from pydub import effects
 
-I = logging.getLogger("pydub.converter")
+I = logging.getLogger("pydub.normalizer")
 I.setLevel(logging.DEBUG)
 I.addHandler(logging.StreamHandler())
 
@@ -11,5 +12,5 @@ class Normalizer():
     def __init__(self):
         self.audio_path = audio_path
 
-    def normalize():
-        pass
+    def normalize(self,audio_path):
+        
