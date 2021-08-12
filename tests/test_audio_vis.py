9e2de44e669305ebce0bf8937d4ae14ee924d508
behigu <be.gizachew@gmail.com>
2021-08-12 19:10:46 +0300

    create the test class

--- tests/test_audio_vis.py
+++ tests/test_audio_vis.py
@@ -1,1 +1,13 @@
-class
+import os
+import sys
+import unittest
+import numpy as np
+import pandas as pd
+
+
+class TestDfHelper(unittest.TestCase):
+  pass
+
+
+if __name__ == '__main__':
+      unittest.main()
