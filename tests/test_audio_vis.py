501f515b464373f4f96ad551223dee2c401267d9
behigu <be.gizachew@gmail.com>
2021-08-13 18:59:20 +0300

    test script started

---
+++ tests/test_audio_vis.py
@@ -0,0 +1,18 @@
+from audio_vis import AudioVis
+import os
+import sys
+import unittest
+import numpy as np
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../scripts')))
+
+
+class TestAudioVis(unittest.TestCase):
+
+  def setUp(self) -> pd.DataFrame:
+    self.audio_vis = AudioVis()
+
+
+if __name__ == '__main__':
+      unittest.main()
